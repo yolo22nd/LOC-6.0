45e8bcb26c9bcbedaf854752c48312bc38036498
Kaumudi Soman <kaumudi814@gmail.com>
2024-03-09 23:05:33 +0530

    comparison

---
+++ backend/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+class SearchHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SearchHistory
+        fields = ['query', 'timestamp']
+
+class ProductComparisonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comparison
+        fields = ['product1', 'product2', 'timestamp']
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('jwt_auth/', include('api.jwt_auth.urls')),
     path('fetchall/', FetchAllData.as_view(), name='fetch_all'),
-    path('fetchfiltered/', FetchFilteredData.as_view(), name='fetch_filtered')
+    path('fetchfiltered/', FetchFilteredData.as_view(), name='fetch_filtered'),
+    path('compare/', ProductComparison.as_view(), name='compare_products')
 ]
