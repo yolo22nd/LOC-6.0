6ef01f7295123aca9fd3a15c1a70088c0af7b498
Parushii <128318515+Parushii@users.noreply.github.com>
2024-03-10 11:15:37 +0530

    Update Card.js

--- frontend/app/src/components/Card.js
+++ frontend/app/src/components/Card.js
@@ -9,29 +9,18 @@ const Card = ({ title, img, currency, price, discount, onSelect }) => {
   const [isSelected, setIsSelected] = useState(false);
   const [selectedItems, setSelectedItems] = useState([]);
 
-  const defaultOptions = {
-    reverse:        false,  // reverse the tilt direction
-    max:            35,     // max tilt rotation (degrees)
-    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
-    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
-    speed:          1000,   // Speed of the enter/exit transition
-    transition:     true,   // Set a transition on enter/exit.
-    axis:           null,   // What axis should be disabled. Can be X or Y.
-    reset:          true,    // If the tilt effect has to be reset on exit.
-    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-  }
-  let updatedItems={}
   useEffect(() => {
-    console.log("selectedItems length:", updatedItems.length);
+    console.log("selectedItems length:", selectedItems.length);
+    console.log(selectedItems)
   }, [selectedItems]); // Log selectedItems length whenever it changes
 
   const handleCardClick = () => {
     // If selectedItems length is less than 2 or the clicked item is already selected, proceed
-    if (isSelected || updatedItems.length < 2) {
+    if (isSelected || selectedItems.length < 2) {
       setIsSelected(!isSelected);
       // If the item is already selected, remove it from selectedItems
       if (isSelected) {
-         updatedItems = selectedItems.filter((item) => item !== title);
+        const updatedItems = updatedItems.append(selectedItems.filter((item) => item !== title));
         setSelectedItems(updatedItems);
       } else {
         // If the item is not selected, add it to selectedItems
@@ -43,47 +32,48 @@ const Card = ({ title, img, currency, price, discount, onSelect }) => {
       // You might want to add some feedback to the user, e.g., a toast message
     }
   };
-  
 
-  return parameter ? (
-    <div
-      key={title}
-      className={`rounded-lg overflow-hidden shadow-2xl ml-8 w-80 bg-white mb-8 hover:bg-slate-200 cursor-pointer ${
-        isSelected ? "border-2 border-slate-500" : ""
-      }`}
-      onClick={handleCardClick}
-    >
-      <div className="flex justify-center">
-        <img className="h-96" src={img} alt="" />
+  return (
+    parameter ? (
+      <div
+        key={title}
+        className={`rounded-lg overflow-hidden shadow-2xl ml-8 w-80 bg-white mb-8 hover:bg-slate-200 cursor-pointer ${
+          isSelected ? "border-2 border-slate-500" : ""
+        }`}
+        onClick={handleCardClick}
+      >
+        <div className="flex justify-center">
+          <img className="h-96" src={img} alt="" />
+        </div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">{title}</div>
+          <p>
+            <span className="font-bold">Price: </span>
+            <span>{currency}</span>
+            <span>{price}</span>
+          </p>
+          <p className="font-semibold text-red-500">{discount}</p>
+        </div>
       </div>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-black">{title}</div>
-        <p>
-          <span className="font-bold">Price: </span>
-          <span>{currency}</span>
-          <span>{price}</span>
-        </p>
-        <p className="font-semibold text-red-500">{discount}</p>
+    ) : (
+      <div
+        key={title}
+        className="rounded-lg overflow-hidden shadow-2xl ml-8 w-80 bg-white mb-8"
+      >
+        <div className="flex justify-center">
+          <img className="h-96" src={img} alt="" />
+        </div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-black">{title}</div>
+          <p>
+            <span className="font-bold">Price: </span>
+            <span>{currency}</span>
+            <span>{price}</span>
+          </p>
+          <p className="font-semibold text-red-500">{discount}</p>
+        </div>
       </div>
-    </div>
-  ) : (
-    <div
-      key={title}
-      className="rounded-lg overflow-hidden shadow-2xl ml-8 w-80 bg-white mb-8"
-    >
-      <div className="flex justify-center">
-        <img className="h-96" src={img} alt="" />
-      </div>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2 text-black">{title}</div>
-        <p>
-          <span className="font-bold">Price: </span>
-          <span>{currency}</span>
-          <span>{price}</span>
-        </p>
-        <p className="font-semibold text-red-500">{discount}</p>
-      </div>
-    </div>
+    )
   );
 };
 
