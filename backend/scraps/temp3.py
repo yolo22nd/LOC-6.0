073341ad20ad3abac3360b6b6e70605187a1b1dc
yolo22nd <omtank22@gmail.com>
2024-03-10 09:45:00 +0530

    ntemo

--- backend/scraps/temp3.py
+++ backend/scraps/temp3.py
@@ -35,196 +35,138 @@ def scrape_flipkart_search():
 
     response = requests.get(base_url, params=params)
     response.raise_for_status()
+    print(response)
 
     soup = BeautifulSoup(response.content, "html.parser")
+    # print(soup)
     search_results = soup.find_all("div", {"class": "_1AtVbE"})
 
     counter = 0  # Counter to keep track of non-advertisement products
     results = []
 
     for result in search_results:
-        if counter >= 40:
+        print(result)
+        if counter >= 10:
             break  # Stop iterating once 5 non-advertisement products are found
 
-        if result.find("div", class_=["_1xHGtK _373qXS", "_4ddWXP"]):
-            inner_products = result.find_all("div", class_=["_1xHGtK _373qXS", "_4ddWXP"])
+        # if result.find("div", class_=["_1xHGtK _373qXS", "_4ddWXP"]):
+        inner_products = result.find_all("div", class_=["_1xHGtK _373qXS", "_4ddWXP"])
+        print(inner_products)
 
-            for inner_product in inner_products:
+        for inner_product in inner_products:
             
-                ad_flag_inner = inner_product.find("div", class_=["_2I5qvP", "_4HTuuX"])
+            ad_flag_inner = inner_product.find("div", class_=["_2I5qvP", "_4HTuuX"])
 
-                if not ad_flag_inner:  # Check if the inner product is an advertisement
-                    title_element_inner = inner_product.find("a", class_=["IRpwTa", "s1Q9rs"])
-                    if not title_element_inner:
-                        continue
+            if not ad_flag_inner:  # Check if the inner product is an advertisement
+                title_element_inner = inner_product.find("a", class_=["IRpwTa", "s1Q9rs"])
+                if not title_element_inner:
+                    continue
+                href_element_inner = inner_product.find("a", class_=["_2UzuFa", "s1Q9rs"])
+                if not href_element_inner:
+                    continue
+                print("href")
+                href_inner = urljoin("https://www.flipkart.com", href_element_inner["href"])
 
-                    title_inner = title_element_inner.text.strip()
+                temp = inner_page_details(href_inner)
 
-                    price_element_inner = inner_product.find("div", {"class": "_30jeq3"})
-                    if not price_element_inner:
-                        continue
-                    
-                    price_list = []
-                    price_inner = price_element_inner.text.strip()
-                    price_list.append(price_inner)
+                rating_element = result.find("div", class_=["_3LWZlK _1rdVr6 _1BLPMq", "_3LWZlK _1BLPMq", "_3LWZlK _32lA32 _1BLPMq", "_3LWZlK"])
+                if rating_element:
+                    rating = rating_element.text.strip()
+                else:
+                    continue
 
-                    href_element_inner = inner_product.find("a", class_=["_2UzuFa", "s1Q9rs"])
-                    if not href_element_inner:
-                        continue
+                temp['rating']=rating
+                print(temp)
+                products.append(temp)
 
-                    href_inner = urljoin("https://www.flipkart.com", href_element_inner["href"])
+                counter += 1
 
-                    # img_div_inner = inner_product.find('div', attrs={'class': 'CXW8mj'})
-                    # img_tag_inner = img_div_inner.find('img', attrs={'class': '_396cs4'})
-                    # if not img_tag_inner:
-                    #     continue
-
-                    # img_url_inner = img_tag_inner.get('src')
-
-                    # rating_element_inner = inner_product.find("div", class_=["_3LWZlK _1rdVr6 _1BLPMq", "_3LWZlK _1BLPMq", "_3LWZlK _32lA32 _1BLPMq", "_3LWZlK"])
-                    # if rating_element_inner:
-                    #     rating_inner = rating_element_inner.text.strip()
-                    # else:
-                    #     continue
-                    temp=function
-                    products.append(temp)
-
-                    counter += 1
-
-                    # Fetching offers from the URL
-                    # response_inner = requests.get(href_inner)
-                    # soup_inner = BeautifulSoup(response_inner.content, "html.parser")
-                    # offers = soup_inner.find_all("li", {"class": "_16eBzU col"})
-                    # for offer in offers:
-                    #     results[-1].setdefault("offers", []).append(offer.text.strip())
-                    # reviews = soup_inner.find("span", {"class": "_2_R_DZ"})
-                    # if reviews:
-                    #     reviews_text = reviews.text.strip()
-                    #     reviews_number = reviews_text.split("&")[-1].strip()
-                    #     results[-1]["reviews"] = reviews_number
-                    # cut_price_element = soup_inner.find("div", {"class": "_3I9_wc _2p6lqe"})
-                    # if cut_price_element:
-                    #     cut_price = cut_price_element.text.strip()
-                    #     results[-1]["cut_price"] = cut_price
-
-                    # #coupons
-                    # coupon_price_list = []
-                    # for prices in price_list:
-                    #     without_special_symbol = prices.removeprefix("₹").replace(",", "")
-                    #     price_int = int(without_special_symbol)
-                    #     coupon_price_float = price_int * 0.05
-                    #     coupon_price_list.append(str(round(coupon_price_float)))
-
-                    # coupon_price_singal_digit = []
-                    # for i in coupon_price_list:
-                    #     for char in i:
-                    #         coupon_price_singal_digit.append(char)
-                    # coupon_price_val = coupon_price(coupon_price_singal_digit)
-                    
-                    # results[-1]['after 5%'] = coupon_price_list
-                    # results[-1]['coupon_val'] = coupon_price_val
-
-        # else:
-        #     ad_flag = result.find("div", {"class": "_2tfzpE"})
-
-        #     if not ad_flag:  # Check if the result is an advertisement
-        #         title_element = result.find("div", {"class": "_4rR01T"})
-        #         if not title_element:
-        #             continue
-
-        #         title = title_element.text.strip()
-
-        #         price_list = []
-        #         price_element = result.find("div", {"class": "_30jeq3"})
-        #         print(price_element)
-        #         if not price_element:
-        #             continue
-
-        #         price = price_element.text.strip()
-        #         # print(price)
-        #         price_list.append(price)
-
-        #         href_element = result.find("a", {"class": "_1fQZEK"})
-        #         if not href_element:
-        #             continue
-
-        #         href = urljoin("https://www.flipkart.com", href_element["href"])
-
-        #         img_div = result.find('div', attrs={'class': 'CXW8mj'})
-        #         img_tag = img_div.find('img', attrs={'class': '_396cs4'})
-        #         if not img_tag:
-        #             continue
-
-        #         img_url = img_tag.get('src')
-
-        #         rating_element = result.find("div", class_=["_3LWZlK _1rdVr6 _1BLPMq", "_3LWZlK _1BLPMq", "_3LWZlK _32lA32 _1BLPMq", "_3LWZlK"])
-        #         if rating_element:
-        #             rating = rating_element.text.strip()
-        #         else:
-        #             continue
-
-        #         results.append({
-        #             'id':counter,
-        #             "platform": "Flipkart",
-        #             "title": title,
-        #             "price": price,
-        #             "href": href,
-        #             "img_url": img_url,
-        #             "rating": rating
-        #         })
-                
-
-        #         counter += 1
-
-                # Fetching offers from the URL
-                # response_inner = requests.get(href)
-                # soup_inner = BeautifulSoup(response_inner.content, "html.parser")
-                # offers = soup_inner.find_all("li", {"class": "_16eBzU col"})
-                # for offer in offers:
-                #     results[-1].setdefault("offers", []).append(offer.text.strip())
-                # reviews = soup_inner.find("span", {"class": "_2_R_DZ"})
-                # if reviews:
-                #     reviews_text = reviews.text.strip()
-                #     reviews_number = reviews_text.split("&")[-1].strip()
-                #     results[-1]["reviews"] = reviews_number
-
-                # cut_price_element = soup_inner.find("div", {"class": "_3I9_wc _2p6lqe"})
-                # if cut_price_element:
-                #     cut_price = cut_price_element.text.strip()
-                #     results[-1]["cut_price"] = cut_price
-
-                # #coupons
-                # coupon_price_list = []
-                # for prices in price_list:
-                #     without_special_symbol = prices.removeprefix("₹").replace(",", "")
-                #     price_int = int(without_special_symbol)
-                #     coupon_price_float = price_int * 0.05
-                #     coupon_price_list.append(str(round(coupon_price_float)))
-                # print(coupon_price_list)
-
-                # coupon_price_singal_digit = []
-                # for i in coupon_price_list:
-                #     for char in i:
-                #         coupon_price_singal_digit.append(char)
-                # print(coupon_price_singal_digit)
-                # coupon_price_val = coupon_price(coupon_price_singal_digit)
-                
-                # results[-1]['after 5%'] = coupon_price_list
-                # results[-1]['coupon_val'] = coupon_price_val
-                print(products[-1])
-    # print(results)
-
-
-    # products=[
-    # {'website': 'amazon', 'title': 'Apple iPhone 13 (256GB) - (Product) RED', 'price': '61,900', 'img': ['https://m.media-amazon.com/images/I/315oQlfQ6WL._SY445_SX342_QL70_ML2_.jpg'], 'discount': '-11%', 'about': ['', '', '', '', '', ''], 'rating': '4.6 out of 5 stars', 'link': 'https://www.amazon.in/Apple-iPhone-13-128GB-Green/dp/B09G9HDN4Q/ref=sr_1_1?crid=1EA3SCHCHDZF2&dib=eyJ2IjoiMSJ9.lylV35R4vlcik0aaO6wOgUefKEwUaeYWK_OuM494a4M2jxaiHGLt-9jMmKFSoG5Orh4Y7OSuWxfkGXRCMvTHdohYmhkhicOTJnl7Pyk2HcGupNeJu_-_GgEuV6Se1pwcIUJZUsGXv1xb_Cp60U71vNzMMC9J0ZWPhbQSVsIzCqUjIv4xGv3AdFxIeiubIVLa.FP_WPgel0NsZggtK-G1O5U14rn_TxPwGLAHspRzOp7w&dib_tag=se&keywords=Apple%2BiPhone%2BXS%2B(Space%2BGrey%2C%2B64%2BGB)&qid=1709968267&sprefix=apple%2Biphone%2Bxs%2Bspace%2Bgrey%2C%2B64%2Bgb%2B%2Caps%2C322&sr=8-1&th=1', 'variations': ['', '', '', '', '', ''], 'options': ['https://m.media-amazon.com/images/I/11VmJVS7yrL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11F1Hxzw6qL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11POY6uLQUL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11+wm+cM1LL._SS36_.jpg', 'https://m.media-amazon.com/images/I/01ytk+eqYTL._SS36_.jpg']},
-    # {'website': 'amazon', 'title': 'Apple iPhone 13 (256GB) - (Product) RED', 'price': '61,900', 'img': ['https://m.media-amazon.com/images/I/315oQlfQ6WL._SY445_SX342_QL70_ML2_.jpg'], 'discount': '-11%', 'about': ['', '', '', '', '', ''], 'rating': '4.6 out of 5 stars', 'link': 'https://www.amazon.in/Apple-iPhone-13-128GB-Green/dp/B09G9HDN4Q/ref=sr_1_1?crid=1EA3SCHCHDZF2&dib=eyJ2IjoiMSJ9.lylV35R4vlcik0aaO6wOgUefKEwUaeYWK_OuM494a4M2jxaiHGLt-9jMmKFSoG5Orh4Y7OSuWxfkGXRCMvTHdohYmhkhicOTJnl7Pyk2HcGupNeJu_-_GgEuV6Se1pwcIUJZUsGXv1xb_Cp60U71vNzMMC9J0ZWPhbQSVsIzCqUjIv4xGv3AdFxIeiubIVLa.FP_WPgel0NsZggtK-G1O5U14rn_TxPwGLAHspRzOp7w&dib_tag=se&keywords=Apple%2BiPhone%2BXS%2B(Space%2BGrey%2C%2B64%2BGB)&qid=1709968267&sprefix=apple%2Biphone%2Bxs%2Bspace%2Bgrey%2C%2B64%2Bgb%2B%2Caps%2C322&sr=8-1&th=1', 'variations': ['', '', '', '', '', ''], 'options': ['https://m.media-amazon.com/images/I/11VmJVS7yrL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11F1Hxzw6qL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11POY6uLQUL._SS36_.jpg', 'https://m.media-amazon.com/images/I/11+wm+cM1LL._SS36_.jpg', 'https://m.media-amazon.com/images/I/01ytk+eqYTL._SS36_.jpg']}
-    # ]
-    # Pad the lists in the dictionary so they all have the same length
     df = pd.DataFrame(products)
+    print(df)
 
-    # Save the DataFrame as a CSV file
-    # If the file does not exist, it will be created. If it does exist, the new data will be appended without the header.
-    # df.to_csv("output.csv", mode='a', header=False, index=False)
+
+def inner_page_details(base_url):
+
+    user_agent = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    }
+
+    response = requests.get(base_url, headers=user_agent)
+    soup = BeautifulSoup(response.content, "html.parser")
+    print(response)
+
+    product={}
+    product['website']="flipkart"
+    # print(product["website"])
+
+    product["title"] = soup.find("span", attrs={"class":'B_NuCI'}).text.strip()
+    # print(product["title"])
+
+    price_list = []
+    price_element = soup.find("div", {"class": "_30jeq3"})
+    product["price"] = price_element.text.strip()
+    # print(product["price"])
+
+
+    discount_element = soup.find("div", attrs={"class": "_3Ay6Sb _31Dcoz"})
+    for i in discount_element:
+        product["discount"] = i.text.strip()
+    # print(product["discount"])
+
+
+    cut_price_element = soup.find("div", {"class": "_3I9_wc _2p6lqe"})
+    if cut_price_element:
+        product["cut_price"] = cut_price_element.text.strip()
+    # print(product["cut_price"])
+
+    product_images = []
+    product_imgs = soup.find_all("li", attrs={"class": "_20Gt85 _1Y_A6W"})
+    for i in product_imgs:
+        for j in i:
+            for k in j:
+                for l in k:
+                    product_images.append(l['src'])
+    product["images"] = product_images
+    # print(product["images"])
+
+    titles = []
+    title_elements = soup.find_all("div", attrs={"class": "flxcaE"})
+    for div in title_elements:
+        titles.append(div.text.strip())
+    product["description_titles"] = titles
+    # print(product["description_titles"])
+
+    tables = soup.find_all("table", class_="_14cfVK")
+    table_data_list = []
+    for table in tables:
+        table_data = {}
+        rows = table.find_all("tr")
+        for row in rows:
+            headers = row.find_all("td", class_="_1hKmbr")
+            values = row.find_all("li", class_="_21lJbe")
+            if headers and values:
+                table_data[headers[0].text.strip()] = values[0].text.strip()
+        table_data_list.append(table_data)
+    product["description_info"] = json.dumps(table_data_list, indent=2)
+    # print(product["description_info"])
+
+    desc_div = soup.find("div", {"class":"_1mXcCf RmoJUa"})
+    for i in desc_div:
+        product["about"] = i.text.strip()
+
+
+    product["rating"] = soup.find("div", attrs={"class":'_2d4LTz'}).text.strip()
+    # print(product["rating"])
+
+    product["link"] = base_url
+    # print(product["link"])
+
+    product["variations"] = [li.text.strip() for li in soup.find_all('li', {'class': 'a-spacing-small item'})]
+
+    product["options"] = [img['src'] for img in soup.find_all('img', {'class': 'imgSwatch'})]
+
+    # print(product)
+    return product
 
 
 scrape_flipkart_search()
--- backend/scraps/temp4.py
+++ backend/scraps/temp4.py
@@ -3,19 +3,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import pandas as pd
 import numpy as np
-import time
-
-
-
 
 def inner_page_details(base_url):
-    results=[]
-    # base_url = "https://www.amazon.in/Campus-11G-677-Black-North-Running/dp/B08PSGW8H9/ref=sr_1_4_sspa?crid=256VHHTCGXB4&dib=eyJ2IjoiMSJ9.tE5mNLJoSjmwDC0qCnukyIQWR4HKCMnh5qiaGmmKznWqyjbx_yaFaqQIwmdX8oprDDhHmeseVOnCQTrdIFBcXKXfx-LEGhKR82Wbj-G0XGyhknQKCkF5QzhYt9rid_XLlh386_EvE0f0YmpL5xKeNp5OI1F99NhT2IPyThRyd6jSdM4x_BPXzJzNHheqOy7jutMA7rLUdVgt1PVU0-uhTXjNQs_S82vGWEl2NduahXLmjRimg14933z7G-CciE-7tO4XQwDEDwukQmjNcudIVDeMTjw9jD2FzNLBB05sw2s.jkxGk9tTFZeqjLxZN2oI4B0kNlMfhJIxDm97S1RS6t0&dib_tag=se&keywords=shoes&qid=1709999845&sprefix=shoes%2Caps%2C223&sr=8-4-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&psc=1"
-    # base_url = "https://www.amazon.in/Apple-iPhone-13-128GB-Green/dp/B09G9HDN4Q/ref=sr_1_1?crid=1EA3SCHCHDZF2&dib=eyJ2IjoiMSJ9.lylV35R4vlcik0aaO6wOgUefKEwUaeYWK_OuM494a4M2jxaiHGLt-9jMmKFSoG5Orh4Y7OSuWxfkGXRCMvTHdohYmhkhicOTJnl7Pyk2HcGupNeJu_-_GgEuV6Se1pwcIUJZUsGXv1xb_Cp60U71vNzMMC9J0ZWPhbQSVsIzCqUjIv4xGv3AdFxIeiubIVLa.FP_WPgel0NsZggtK-G1O5U14rn_TxPwGLAHspRzOp7w&dib_tag=se&keywords=Apple%2BiPhone%2BXS%2B(Space%2BGrey%2C%2B64%2BGB)&qid=1709968267&sprefix=apple%2Biphone%2Bxs%2Bspace%2Bgrey%2C%2B64%2Bgb%2B%2Caps%2C322&sr=8-1&th=1"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-
     user_agent = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     }
@@ -43,14 +32,7 @@ def inner_page_details(base_url):
 
     temp = soup.find_all("div", attrs={"class":"a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"})
     print(temp)
-    # for i in temp:
-    #     print(i)
-    # product["discount"] = 
-    # temp = soup.get_elent
 
-
-
-    xpath='/html/body/div[2]/div/div[5]/div[8]/div[7]/div/div[1]/ul/li[1]/span/span[1]/text()'
 
     product["img"] = [img['src'] for img in soup.find_all('img', {'class': 'a-dynamic-image'})]
     # print(product["img"])
@@ -140,50 +122,24 @@ def scrape_amazon_search(query):
                 else:
                     reviews = 'No reviews'
                 temp['reviews']=reviews
+                print(temp)
+                df = pd.DataFrame([temp])
+                # Append the DataFrame to the CSV file
+                # df.to_csv("output.csv", mode='a', header=False, index=False)
+
                 
-
-                # bank_offer_num = soup.find('div', attrs={'id': 'itembox-InstantBankDiscount'})
-                # if bank_offer_num is not None:
-                #     bank_offer_num_txt = bank_offer_num.find('a', attrs={'class': 'a-size-base a-link-emphasis vsx-offers-count'}).string.strip().split(" ")[1]
-                # else:
-                #     bank_offer_num_txt = ""
-
-                # bank_offer_url = f"https://www.amazon.in/hctp/vsxoffer?asin={asin}&deviceType=web&offerType=InstantBankDiscount&buyingOptionIndex"
-                # time.sleep(2)
-                # result=[]
-                # try:
-                #     request_bank_offer = requests.get(bank_offer_url, headers=user_agent)
-                #     request_bank_offer.raise_for_status()
-                #     soup_bank_offer = BeautifulSoup(request_bank_offer.content, 'html.parser')
-                    
-                #     if bank_offer_num_txt == 'offers':
-                #         bank_offers = soup_bank_offer.find_all('p', attrs={'class': 'a-spacing-mini a-size-base-plus'})
-                #         result["offers"] = [offer.get_text() for offer in bank_offers]
-                #     else:
-                #         bank_offer = soup_bank_offer.find_all('h1', attrs={'class': 'a-size-medium-plus a-spacing-medium a-spacing-top-small'})
-                #         result["offers"] = [offer.get_text().strip() for offer in bank_offer]
-
-                #     temp['offers']=result
-
-                #     counter += 1
-                #     if counter >= 5:
-                #         break
-
-                # except requests.exceptions.HTTPError as e:
-                #     print(f"Error fetching bank offers for URL: {bank_offer_url}")
-                #     print(f"Exception: {str(e)}")
                 
         page += 1
-        print(temp[-1])
+        # print(temp[-1])
 
-    return temp
+    # return temp
 
 
-newnew=scrape_amazon_search(input("enter search query"))
+scrape_amazon_search(input("enter search query"))
 
-df = pd.DataFrame(newnew)
+# df = pd.DataFrame(newnew)
 
-    # Save the DataFrame as a CSV file
-    # If the file does not exist, it will be created. If it does exist, the new data will be appended without the header.
-    # df.to_csv("output.csv", mode='a', header=False, index=False)
+# Save the DataFrame as a CSV file
+# If the file does not exist, it will be created. If it does exist, the new data will be appended without the header.
+# df.to_csv("output.csv", mode='a', header=False, index=False)
 
